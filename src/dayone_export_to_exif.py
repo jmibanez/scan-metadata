62d1df9bfb74787995827ca3361b7371b5d79f82
JM Ibanez <jm@jmibanez.com>
2025-03-30 23:35:05 +1100

    Handle case where explicit location info missing in Day One export
    If these fields don't exist, just continue.

--- src/dayone_export_to_exif.py
+++ src/dayone_export_to_exif.py
@@ -79,8 +79,10 @@ class MetadataEntry(object):
             self.add_exif_tag("GPSLongitudeRef", lon)
             self.add_exif_tag("GPSHPositioningError", radius)
 
-        self.add_exif_tag("Country", self.location["country"])
-        self.add_exif_tag("State", self.location["administrativeArea"])
+        if "country" in self.location:
+            self.add_exif_tag("Country", self.location["country"])
+        if "administrativeArea" in self.location:
+            self.add_exif_tag("State", self.location["administrativeArea"])
 
 
     def populate_from_entry_tags(self):
