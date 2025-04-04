f7211b4b939de5d937e605976c46a5db758b4011
JM Ibanez <jm@jmibanez.com>
2025-04-04 17:27:42 +1100

    Add experimental internal EXIF processing support
    Add support for manipulating EXIF/IPTC/XMP metadata in process via
    rexiv2. Because the tag names are different between exiftool and
    rexiv2 (exiv2), we need to provide a translation table (for now)
    between the two.

--- src/exif.rs
+++ src/exif.rs
@@ -1,4 +1,8 @@
+use lazy_static::lazy_static;
 use num::rational::Ratio;
+use rexiv2::{set_log_level, LogLevel, Metadata};
+
+use std::collections::HashMap;
 use std::path::Path;
 use std::process::Command;
 
@@ -160,18 +164,128 @@ impl ExifProcessor for ExifToolProcessor {
     }
 }
 
+lazy_static! {
+    static ref TAG_TRANSLATIONS: HashMap<&'static str, &'static str> = {
+        let mut m = HashMap::new();
+        m.insert("DateTimeOriginal", "Exif.Photo.DateTimeOriginal");
+        m.insert("GPSLatitude", "Exif.GPSInfo.GPSLatitude");
+        m.insert("GPSLongitude", "Exif.GPSInfo.GPSLongitude");
+        m.insert("GPSHPositioningError", "Exif.GPSInfo.GPSHPositioningError");
+        m.insert("Country", "Xmp.iptcExt.CountryName");
+        m.insert("State", "Xmp.iptcExt.ProvinceState");
+        m.insert("FNumber", "Exif.Photo.FNumber");
+        m.insert("ExposureTime", "Exif.Photo.ExposureTime");
+        m.insert("MaxAperture", "Xmp.exif.MaxApertureValue");
+        m.insert("FocalLength", "Exif.Photo.FocalLength");
+        m.insert("LensMake", "Xmp.exifEX.LensMake");
+        m.insert("LensModel", "Xmp.exifEX.LensModel");
+        m.insert("CameraLabel", "Xmp.xmpDM.cameraLabel");
+        m.insert("UserComment", "Exif.Photo.UserComment");
+        m.insert("Keywords", "Xmp.iptc.Application2.Keywords");
+        m
+    };
+}
+
+fn translate_tag_to_exiv(tag_name: &str) -> &str {
+    TAG_TRANSLATIONS.get(&tag_name).unwrap_or(&tag_name)
+}
+
 struct ExperimentalExifProcessor {}
+
+impl ExperimentalExifProcessor {
+    fn apply_exif_tag(&self, meta: &Metadata, tag: &ExifTag) {
+        let tag_name = translate_tag_to_exiv(tag.name.as_str());
+        let result = match &tag.value {
+            TagValue::String(v) => meta.set_tag_string(tag_name, v),
+            TagValue::Numeric(v) => meta.set_tag_numeric(tag_name, *v),
+            TagValue::Rational(v) => meta.set_tag_rational(tag_name, v),
+            TagValue::List(l) => {
+                let l_str: Vec<&str> = l.iter().map(|e| e.as_str()).collect();
+                meta.set_tag_multiple_strings(tag_name, l_str.as_slice())
+            }
+        };
+
+        if result.is_err() {
+            let err = result.err().unwrap();
+            println!("Error writing {}: {}", tag_name, err);
+        }
+    }
+}
 
 impl ExifProcessor for ExperimentalExifProcessor {
     fn write_out_exif(
         &self,
         filepath: &Path,
-        _exif_tags: &Vec<ExifTag>,
+        exif_tags: &Vec<ExifTag>,
         options: &ExifProcessorOptions,
     ) -> bool {
         if !options.dryrun {
             println!("EXPERIMENTAL Updating tags for {}", filepath.display());
-            true
+            let meta = Metadata::new_from_path(filepath).unwrap();
+            set_log_level(LogLevel::MUTE);
+
+            let mut lens_spec: [String; 4] = Default::default();
+            for tag in exif_tags.iter() {
+                if tag.name == "MinFocalLength" {
+                    if let TagValue::String(s) = &tag.value{
+                        lens_spec[0] = s.clone()
+                    }
+                }
+                if tag.name == "MaxFocalLength" {
+                    if let TagValue::String(s) = &tag.value{
+                        lens_spec[1] = s.clone()
+                    }
+                }
+                if tag.name == "MaxApertureAtMinFocal"   {
+                    if let TagValue::String(s) = &tag.value{
+                        lens_spec[2] = s.clone()
+                    }
+                }
+                if tag.name == "MaxApertureAtMaxFocal"   {
+                    if let TagValue::String(s) = &tag.value{
+                        lens_spec[3] = s.clone()
+                    }
+                }
+            }
+
+            let lens_spec_tag = Vec::from(lens_spec).to_exif_tag("Exif.Photo.LensSpecification");
+            self.apply_exif_tag(&meta, &lens_spec_tag);
+
+            for tag in exif_tags.iter() {
+                if tag.name == "MinFocalLength" || tag.name == "MaxFocalLength" || tag.name == "MaxApertureAtMinFocal" || tag.name == "MaxApertureAtMaxFocal" {
+                    continue;
+                }
+                self.apply_exif_tag(&meta, tag);
+            }
+
+            if !options.inplace {
+                use std::fs::copy;
+                let mut new_name = filepath.file_name().unwrap().to_os_string();
+                new_name.push("_original");
+                let newpath = filepath.with_file_name(new_name);
+                match copy(filepath, newpath) {
+                    Ok(_) => (),
+                    Err(e) => {
+                        println!(
+                            "WARNING: Could not preserve original {}, falling back to in-place: {}",
+                            filepath.display(),
+                            e
+                        );
+                    }
+                }
+            }
+
+            match meta.save_to_file(filepath) {
+                Ok(()) => true,
+                Err(e) => {
+                    println!(
+                        "WARNING: Could not update metadata for scan {}: {}",
+                        filepath.display(),
+                        e
+                    );
+                    false
+                }
+            }
         } else {
             println!("EXPERIMENTAL Would have updated {}", filepath.display());
             true
