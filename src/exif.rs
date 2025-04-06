b43bdb2800a985b669e38aa1bc3ef8f626abc37e
JM Ibanez <jm@jmibanez.com>
2025-04-07 03:10:36 +1000

    Increase tests and test coverage
    This is starting to become more complex, with an increasing number of
    moving parts -- the internal EXIF processor bits in particular have
    some special casing that will evolve as I refactor things.
    
    Increase the test coverage to avoid regressions in behavior.

--- src/exif.rs
+++ src/exif.rs
@@ -1,7 +1,7 @@
 use lazy_static::lazy_static;
 use log::{debug, warn, LevelFilter};
 use num::rational::Ratio;
-use rexiv2::Metadata;
+use rexiv2::{GpsInfo, Metadata};
 
 use std::collections::{HashMap, HashSet};
 use std::path::Path;
@@ -220,10 +220,41 @@ fn translate_tag_to_exiv(tag_name: &str) -> &str {
     TAG_TRANSLATIONS.get(&tag_name).unwrap_or(&tag_name)
 }
 
+#[cfg_attr(test, mockall::automock)]
+trait MetadataOperations {
+    fn set_tag_string(&self, tag: &str, value: &str) -> rexiv2::Result<()>;
+    fn set_tag_multiple_strings<'a>(&self, tag: &str, values: &'a [&'a str]) -> rexiv2::Result<()>;
+    fn set_tag_numeric(&self, tag: &str, value: i32) -> rexiv2::Result<()>;
+    fn set_tag_rational(&self, tag: &str, value: &Ratio<i32>) -> rexiv2::Result<()>;
+    fn set_gps_info(&self, gps: &GpsInfo) -> rexiv2::Result<()>;
+}
+
+impl MetadataOperations for Metadata {
+    fn set_tag_string(&self, tag: &str, value: &str) -> rexiv2::Result<()> {
+        self.set_tag_string(tag, value)
+    }
+
+    fn set_tag_multiple_strings(&self, tag: &str, values: &[&str]) -> rexiv2::Result<()> {
+        self.set_tag_multiple_strings(tag, values)
+    }
+
+    fn set_tag_numeric(&self, tag: &str, value: i32) -> rexiv2::Result<()> {
+        self.set_tag_numeric(tag, value)
+    }
+
+    fn set_tag_rational(&self, tag: &str, value: &Ratio<i32>) -> rexiv2::Result<()> {
+        self.set_tag_rational(tag, value)
+    }
+
+    fn set_gps_info(&self, gps: &GpsInfo) -> rexiv2::Result<()> {
+        self.set_gps_info(gps)
+    }
+}
+
 struct ExperimentalExifProcessor {}
 
 impl ExperimentalExifProcessor {
-    fn apply_exif_tag(&self, meta: &Metadata, tag: &ExifTag) {
+    fn apply_exif_tag(&self, meta: &dyn MetadataOperations, tag: &ExifTag) {
         let tag_name = translate_tag_to_exiv(tag.name.as_str());
         let result = match &tag.value {
             TagValue::String(v) => meta.set_tag_string(tag_name, v),
@@ -245,7 +276,7 @@ impl ExperimentalExifProcessor {
         }
     }
 
-    fn handle_special_case_tags(&self, meta: &Metadata, exif_tags: &[ExifTag]) {
+    fn handle_special_case_tags(&self, meta: &dyn MetadataOperations, exif_tags: &[ExifTag]) {
         // Special case tags here
 
         let mut maybe_latitude: Option<f64> = None;
@@ -324,7 +355,7 @@ impl ExperimentalExifProcessor {
         }
 
         if let (Some(latitude), Some(longitude)) = (maybe_latitude, maybe_longitude) {
-            match meta.set_gps_info(&rexiv2::GpsInfo {
+            match meta.set_gps_info(&GpsInfo {
                 latitude,
                 longitude,
                 altitude: 0.0,
@@ -402,4 +433,254 @@ pub fn get_default_processor() -> impl ExifProcessor {
 
 pub fn get_experimental_processor() -> impl ExifProcessor {
     ExperimentalExifProcessor {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockall::predicate::*;
+
+    fn fake_exif_tags() -> Vec<ExifTag> {
+        let mut exif_tags = Vec::new();
+        exif_tags.push("bar".to_exif_tag("Foo"));
+        exif_tags.push("hey".to_exif_tag("Hey"));
+        exif_tags.push(42.to_exif_tag("SomeInt"));
+
+        exif_tags
+    }
+
+    #[test]
+    fn exiftool_processor_should_yield_correct_cmd_line() {
+        let proc = ExifToolProcessor {};
+        let exif_tags = fake_exif_tags();
+
+        assert_eq!(
+            Vec::from([
+                "-q",
+                "-overwrite_original_in_place",
+                "-Foo=bar",
+                "-Hey=hey",
+                "-SomeInt=42",
+                "/test/path"
+            ]),
+            proc.to_exiftool_cmd_line(
+                Path::new("/test/path"),
+                &exif_tags,
+                &ExifProcessorOptions {
+                    inplace: true,
+                    dryrun: true,
+                },
+            )
+        );
+
+        assert_eq!(
+            Vec::from([
+                "-q",
+                "-overwrite_original_in_place",
+                "-Foo=bar",
+                "-Hey=hey",
+                "-SomeInt=42",
+                "/test/path"
+            ]),
+            proc.to_exiftool_cmd_line(
+                Path::new("/test/path"),
+                &exif_tags,
+                &ExifProcessorOptions {
+                    inplace: true,
+                    dryrun: false,
+                },
+            )
+        );
+
+        assert_eq!(
+            Vec::from(["-q", "-Foo=bar", "-Hey=hey", "-SomeInt=42", "/test/path"]),
+            proc.to_exiftool_cmd_line(
+                Path::new("/test/path"),
+                &exif_tags,
+                &ExifProcessorOptions {
+                    inplace: false,
+                    dryrun: false,
+                },
+            )
+        );
+    }
+
+    #[test]
+    fn exiftool_processor_should_handle_various_value_types() {
+        let proc = ExifToolProcessor {};
+        let mut exif_tags = Vec::new();
+        exif_tags.push("bar".to_exif_tag("Foo"));
+        exif_tags.push("hey".to_string().to_exif_tag("Hey"));
+        exif_tags.push(42.to_exif_tag("SomeInt"));
+        exif_tags.push((42.42).to_exif_tag("SomeFloat"));
+        let ratio = Ratio::new(3, 7);
+        exif_tags.push(ratio.to_exif_tag("SomeRatio"));
+
+        assert_eq!(
+            Vec::from([
+                "-q",
+                "-Foo=bar",
+                "-Hey=hey",
+                "-SomeInt=42",
+                "-SomeFloat=42.42",
+                "-SomeRatio=3/7",
+                "/test/path"
+            ]),
+            proc.to_exiftool_cmd_line(
+                Path::new("/test/path"),
+                &exif_tags,
+                &ExifProcessorOptions {
+                    inplace: false,
+                    dryrun: true,
+                },
+            )
+        );
+    }
+
+    #[test]
+    fn rexiv2_processor_apply_exif_tag_base_case() {
+        let proc = ExperimentalExifProcessor {};
+        let mut mock_meta = MockMetadataOperations::new();
+
+        mock_meta
+            .expect_set_tag_string()
+            .once()
+            .returning(|_, _| Ok(()))
+            .with(eq("TestTag"), eq("TestTagValue"));
+        mock_meta.expect_set_tag_numeric().never();
+        mock_meta.expect_set_tag_rational().never();
+        mock_meta.expect_set_tag_multiple_strings().never();
+
+        let tag = "TestTagValue".to_exif_tag("TestTag");
+
+        proc.apply_exif_tag(&mock_meta, &tag);
+    }
+
+    #[test]
+    fn rexiv2_processor_apply_exif_tag_on_translated_string_tag() {
+        let proc = ExperimentalExifProcessor {};
+        let mut mock_meta = MockMetadataOperations::new();
+
+        mock_meta
+            .expect_set_tag_string()
+            .once()
+            .returning(|_, _| Ok(()))
+            .with(eq("Exif.Photo.DateTimeOriginal"), eq("TestTagValue"));
+        mock_meta.expect_set_tag_numeric().never();
+        mock_meta.expect_set_tag_rational().never();
+        mock_meta.expect_set_tag_multiple_strings().never();
+
+        let tag = "TestTagValue".to_exif_tag("DateTimeOriginal");
+
+        proc.apply_exif_tag(&mock_meta, &tag);
+    }
+
+    #[test]
+    fn rexiv2_processor_ignore_regular_tags_in_handle_special_case_tags() {
+        let proc = ExperimentalExifProcessor {};
+        let mut mock_meta = MockMetadataOperations::new();
+
+        let foo_tag = (-1.234).to_exif_tag("Foo");
+        let bar_tag = (-5.6789).to_exif_tag("Bar");
+
+        mock_meta.expect_set_tag_string().never();
+        mock_meta.expect_set_tag_numeric().never();
+        mock_meta.expect_set_tag_rational().never();
+        mock_meta.expect_set_tag_multiple_strings().never();
+        mock_meta.expect_set_gps_info().never();
+
+        proc.handle_special_case_tags(&mock_meta, &[foo_tag, bar_tag]);
+    }
+
+    #[test]
+    fn rexiv2_processor_special_casing_ignore_gps_tags_if_either_missing() {
+        let proc = ExperimentalExifProcessor {};
+        let mut mock_meta = MockMetadataOperations::new();
+
+        mock_meta.expect_set_gps_info().never();
+        mock_meta.expect_set_tag_string().never();
+        mock_meta.expect_set_tag_numeric().never();
+        mock_meta.expect_set_tag_rational().never();
+        mock_meta.expect_set_tag_multiple_strings().never();
+
+        let lon = (-1.234).to_exif_tag("GPSLongitude");
+        let lat = (-5.6789).to_exif_tag("GPSLatitude");
+
+        proc.handle_special_case_tags(&mock_meta, &[lon]);
+
+        mock_meta.checkpoint();
+
+        proc.handle_special_case_tags(&mock_meta, &[lat]);
+    }
+
+    #[test]
+    fn rexiv2_processor_handle_special_cased_gps_tags() {
+        let proc = ExperimentalExifProcessor {};
+        let mut mock_meta = MockMetadataOperations::new();
+
+        mock_meta
+            .expect_set_gps_info()
+            .once()
+            .returning(|_| Ok(()))
+            .with(eq(GpsInfo {
+                longitude: -1.234,
+                latitude: -5.6789,
+                altitude: 0.0,
+            }));
+        mock_meta.expect_set_tag_string().never();
+        mock_meta.expect_set_tag_numeric().never();
+        mock_meta.expect_set_tag_rational().never();
+        mock_meta.expect_set_tag_multiple_strings().never();
+
+        let lon = (-1.234).to_exif_tag("GPSLongitude");
+        let lat = (-5.6789).to_exif_tag("GPSLatitude");
+
+        proc.handle_special_case_tags(&mock_meta, &[lon, lat]);
+    }
+
+    #[test]
+    fn rexiv2_processor_special_casing_ignore_lens_info_tag_when_missing_some() {
+        let proc = ExperimentalExifProcessor {};
+        let mut mock_meta = MockMetadataOperations::new();
+
+        mock_meta.expect_set_gps_info().never();
+        mock_meta.expect_set_tag_string().never();
+        mock_meta.expect_set_tag_numeric().never();
+        mock_meta.expect_set_tag_rational().never();
+        mock_meta.expect_set_tag_multiple_strings().never();
+
+        let min_len = "a".to_exif_tag("MinFocalLength");
+        let max_aperture_min = "c".to_exif_tag("MaxApertureAtMinFocal");
+        let max_aperture_max = "d".to_exif_tag("MaxApertureAtMaxFocal");
+
+        proc.handle_special_case_tags(&mock_meta, &[min_len, max_aperture_min, max_aperture_max]);
+    }
+
+    #[test]
+    fn rexiv2_processor_handle_special_cased_lens_info_tags() {
+        let proc = ExperimentalExifProcessor {};
+        let mut mock_meta = MockMetadataOperations::new();
+
+        mock_meta
+            .expect_set_tag_multiple_strings()
+            .once()
+            .returning(|_, _| Ok(()))
+            .withf(|tag, val| {
+                tag == "Exif.Photo.LensSpecification" && val == vec!["a", "b", "c", "d"]
+            });
+        mock_meta.expect_set_gps_info().never();
+        mock_meta.expect_set_tag_string().never();
+        mock_meta.expect_set_tag_numeric().never();
+        mock_meta.expect_set_tag_rational().never();
+
+        let min_len = "a".to_exif_tag("MinFocalLength");
+        let max_len = "b".to_exif_tag("MaxFocalLength");
+        let max_aperture_min = "c".to_exif_tag("MaxApertureAtMinFocal");
+        let max_aperture_max = "d".to_exif_tag("MaxApertureAtMaxFocal");
+
+        proc.handle_special_case_tags(
+            &mock_meta,
+            &[min_len, max_len, max_aperture_min, max_aperture_max],
+        );
+    }
 }
